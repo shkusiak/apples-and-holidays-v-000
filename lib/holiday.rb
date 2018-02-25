8cbb275b3b5e644eea82542f0fe6992923ec1f68
Shea Kusiak <shkusiak@gmail.com>
2018-02-25 19:33:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,6 +18,11 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash.each do |season, holiday|
+    if season == ":summer"
+      puts "#{holiday}"
+    end
+  end
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
@@ -61,10 +66,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
