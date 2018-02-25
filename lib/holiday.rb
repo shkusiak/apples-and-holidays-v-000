ac09d20e56bfee1333d78f65a6bb03e8f0e84d87
Shea Kusiak <shkusiak@gmail.com>
2018-02-25 23:02:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,7 @@ def all_holidays_with_bbq(holiday_hash)
     holidays_and_supplies.each do | holiday, supplies |
       array =[]
       if supplies.include?("BBQ")
-        array << holiday.to_s
+        array << holiday
         return array
       end
 
