d5e8deae458eaabba66aa3c5a04049d5554edf5b
Shea Kusiak <shkusiak@gmail.com>
2018-02-25 22:57:24 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
   holiday_hash.each do | season, holidays |
-    array_HN = []
+    array_HN = {}
     holidays.each do | holiday, supplies |
 
       if supplies.include?("BBQ")
