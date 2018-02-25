e9024c7cf3a802c348c123579b5891e41bbe0b90
Shea Kusiak <shkusiak@gmail.com>
2018-02-25 22:34:36 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays |
     puts "#{season.capitalize}:"
     holidays.each do |holiday, supplies |
-      holiday.to_s.gsub!("_"," ")
+      holiday.to_s.gsub!(/_/," ")
       puts "  #{holiday.capitalize}: #{supplies.join(", ")}"
     end
   end
