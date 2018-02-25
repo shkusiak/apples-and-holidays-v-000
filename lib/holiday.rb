73fcd219fb01507bf84c943f29f6d19b882fecd9
Shea Kusiak <shkusiak@gmail.com>
2018-02-25 22:06:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,6 +58,12 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each do |season, holidays |
+    puts "#{season}:"
+    holidays.each do |holiday, supply|
+      puts "#{holiday}: #{supply}"
+    end
+  end
 
 end
 
@@ -66,7 +72,8 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
   holiday_hash.each do | season, holidays |
-    puts "#{holidays}"
+    holidays.each do | holiday, supply |
+      if supply.include?
   #  holidays.each do | holiday_name, supply |
       #if supply.include?("BBQ")
       #  array = []
